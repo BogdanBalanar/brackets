6b3196c470bcab0a2945c14f77dd7fd88d73eb72
BogdanBalanar <badikradik123@gmail.com>
2021-03-03 14:18:52 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a=str;
+  let b;
+  for(let i=0;i<bracketsConfig.length;i++){
+    let c=bracketsConfig[i].join('')
+      b = a.replace(c,"")
+      a = b;
+  }
+  if(a==="") return true;
+  else if(a===str) return false;
+  else return(check(a,bracketsConfig))
 }
